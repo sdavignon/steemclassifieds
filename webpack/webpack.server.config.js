15c7c2180d2fd03e14c516a1bc2cce1c802e6fc5
sdavignon <sdavignon1@gmail.com>
2018-02-09 17:13:28 -0500

    Update webpack.server.config.js

--- webpack/webpack.server.config.js
+++ webpack/webpack.server.config.js
@@ -43,10 +43,10 @@ module.exports = {
     new webpack.DefinePlugin({
       'process.env.IMG_HOST': JSON.stringify(process.env.IMG_HOST || 'https://img.busy.org'),
       'process.env.STEEMCONNECT_CLIENT_ID': JSON.stringify(
-        process.env.STEEMCONNECT_CLIENT_ID || 'steemclassifieds.app',
+        process.env.STEEMCONNECT_CLIENT_ID || 'steemclassifieds',
       ),
       'process.env.STEEMCONNECT_REDIRECT_URL': JSON.stringify(
-        process.env.STEEMCONNECT_REDIRECT_URL || 'http://localhost:3000/callback',
+        process.env.STEEMCONNECT_REDIRECT_URL || 'https://steemclassifieds.herokuapp.com/callback',
       ),
       'process.env.STEEMCONNECT_HOST': JSON.stringify(
         process.env.STEEMCONNECT_HOST || 'https://steemconnect.com',
