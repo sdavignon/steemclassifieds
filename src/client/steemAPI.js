035a2199a893586d536735b73779d562e0b68469
sdavignon <sdavignon1@gmail.com>
2018-02-14 06:41:15 -0500

    worth a try

--- src/client/steemAPI.js
+++ src/client/steemAPI.js
@@ -1,12 +1,3 @@
-import { createClient } from 'lightrpc';
+import steem from 'steem';
 
-const client = createClient(process.env.STEEMJS_URL || 'https://api.steemit.com');
-client.sendAsync = (message, params) =>
-  new Promise((resolve, reject) => {
-    client.send(message, params, (err, result) => {
-      if (err !== null) return reject(err);
-      return resolve(result);
-    });
-  });
-
-export default client;
+export default steem.api;
