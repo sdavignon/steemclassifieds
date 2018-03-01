f397f483bb01ddd3679f0927f471a937934f1117
sdavignon <sdavignon1@gmail.com>
2018-03-01 07:57:15 -0500

    Update listinghelper.js

--- assets/listinghelper.js
+++ assets/listinghelper.js
@@ -1,12 +1,14 @@
-
+var user = "";
 function listingForm(){
 $(".wait").show();
    		Cookies.set('transfer', '');
-	$('.Listing').append($('#listingtype'));
-  
-	var user = $(".Topnav__user__username")[0].innerText;
+  	var user = $(".Topnav__user__username")[0].innerText;
+	$('.Listing').load( "/listingTypeForm.html", function() {
+    $('input[type=radio][name=id]').change(CheckItemClick());
+    $(".wait").hide();
+});
+ 
 
-$(".wait").hide();
 }
 
 
@@ -21,18 +23,11 @@ function CheckItemClick(e){
 		Cookies.set($(e).name,$(e).val()); 
 		$(e).addClass("glow");      //add the class to the clicked element
 		localStorage.setItem($(e).name,$(e).val());
- $( "p" ).slideToggle( "slow" );
+    $( "p" ).slideToggle( "slow" );
     $(".Action").data('next') = $(e).data('next'); 
 }	
 
-$(window).scroll(function() {
-    var y = $(".waiting").offset().top;
-    var scrollY = $(window).scrollTop();
-    if (scrollY > y) {
-        var padY = scrollY - y;
-        $(".waiting").css("paddingTop", padY);
-    }
-});
+
 function finishStory() { 
  $('.Action').attr("disabled", "disabled");
   $('.Action').innerText = "Hold On!!";
