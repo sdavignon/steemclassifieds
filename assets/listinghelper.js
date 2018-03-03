ce73e4b6f0379f5174cc817f34172bd493aa8d99
sdavignon <sdavignon1@gmail.com>
2018-03-02 20:23:15 -0500

    Update listinghelper.js

--- assets/listinghelper.js
+++ assets/listinghelper.js
@@ -1,26 +1,26 @@
 var user = "";
+
 function listingForm(){
-localStorage.setItem('id','');
+    localStorage.setItem('tags','');
+    localStorage.setItem('current', 'listingTypeForm');
     $(".waiting").show();
   	localStorage.setItem('transfer', '');
-  	var user = $(".Topnav__user__username")[0].innerText;
+  	user = $(".Topnav__user__username")[0].innerText;
+  
 	  $('.Listing').load( '/listingTypeForm.html', function() {
-      
-      $('input[type=radio][name=id]').each(function () {
-
+       $('input[type=radio][name=id]').each(function () {
        $(this).change(function () {
               CheckItemClick(this);
           });
-       });
- 
-    $(".waiting").hide();
-       $(".Action").click(nextForm);
-
+      });
+      $(".waiting").hide();
+      $("#next").click(nextForm);
+      $("#back").click(lastForm);
     });
-
 }
 
 window.onload = listingForm();
+/*
 $(document).ready(function() {	
 setTimeout(function(){
 	listingForm();
@@ -28,41 +28,50 @@ setTimeout(function(){
   
  }, 1000);
 });
-
+*/
 
 function CheckItemClick(e){
- // $('input[type=radio][name=id]').each(function () {
- //     $(this).parent.removeClass("glow");
- //    });
-  //  $(e).parent.addClass("glow");      //add the class to the clicked element
-    var tags = localStorage.getItem('id');
-  if(tags){tags = tags+' ' + $(e).val();}
-  else{tags = $(e).val();}
-		localStorage.setItem('id',tags);
-  $(".Action").data('next', $(e).data('next'));
- $('.Action').attr("disabled", false);  
+ 
+    var tags = localStorage.getItem('tags');
+
+    if(tags){tags = tags+' ' + $(e).val();}else{tags = $(e).val();}
+    localStorage.setItem('tags',tags);
+
+    $("#back").data('last',  localStorage.getItem('current'));
+    $("#next").data('next', $(e).data('next'));
+
+    $('#next').attr("disabled", false);  
+    localStorage.setItem('current', $(e).data('next'));
 }	
+
 function nextForm() { 
- $('.Action').attr("disabled", true);
- $('.Action').innerText = "Hold On!!";
- $('.waiting').show();
- $('.Listing').load( '/'+ $(".Action").data('next')+'.html', function() {
-      
-      $('input[type=radio][name=id]').each(function () {
+  $('#next').attr("disabled", true);
+  $('#next').innerText = "Hold On!!";
+  $('#back').show();
+  $('.waiting').show();
+  
+  $('.Listing').load('/'+ $("#next").data('next')+'.html', function(){$('input[type=radio][name=id]').each(function(){$(this).change(function () {CheckItemClick(this);});});
 
-       $(this).change(function () {
-              CheckItemClick(this);
-          });
-       });
-    $('.Action').innerText = "Next";
-    $('.Action').attr("disabled", false); 
-    $(".waiting").hide();
-
+  $('#next').innerText = "Next";
+  $('#next').attr("disabled", false); 
+  $(".waiting").hide();
+  $('#back').show();
  });
-
-
  }
+function lastForm() { 
+  $('#back').attr("disabled", true);
+  $('#back').innerText = "Hold On!!";
+  $('#next').show();
+  $('.waiting').show();
+  
+  $('.Listing').load('/'+ $("#back").data('last')+'.html', function(){$('input[type=radio][name=id]').each(function(){$(this).change(function () {CheckItemClick(this);});});
 
+  $('#back').innerText = "Next";
+  $('#back').attr("disabled", false); 
+  $(".waiting").hide();
+  $('#next').show();
+ });
+ }
 
 function finishStory() { 
  $('.Action').attr("disabled", "disabled");
