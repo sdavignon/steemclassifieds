9e061e17a240fa3762b9cc7617a4e43bd893ea79
sdavignon <sdavignon1@gmail.com>
2018-03-03 12:48:58 -0500

    Update Listing.js

--- src/client/listing/Listing.js
+++ src/client/listing/Listing.js
@@ -80,9 +80,9 @@ export default class Listing extends React.Component {
        
             </div>
 <div class="Editor__bottom">
-           <div class="Editor__bottom__left"><div class="ant-row ant-form-item Editor__bottom__back"><div class="ant-form-item-control-wrapper"><div class="ant-form-item-control "></div></div></div><div class="ant-row ant-form-item Editor__bottom__submit"><div class="ant-form-item-control-wrapper"><div class="ant-form-item-control "><button class="Action ant-btn-lg" data-next="listingForm" id="back">Back</button></div></div></div></div>
+           <div class="Editor__bottom__left"><div class="ant-row ant-form-item Editor__bottom__back"><div class="ant-form-item-control-wrapper"><div class="ant-form-item-control "></div></div></div><div class="ant-row ant-form-item Editor__bottom__submit"><div class="ant-form-item-control-wrapper"><div class="ant-form-item-control "><button class="Action ant-btn-lg" data-next="listingTypeForm" id="back">Back</button></div></div></div></div>
 
-            <div class="Editor__bottom__right"><div class="ant-row ant-form-item Editor__bottom__next"><div class="ant-form-item-control-wrapper"><div class="ant-form-item-control "></div></div></div><div class="ant-row ant-form-item Editor__bottom__submit"><div class="ant-form-item-control-wrapper"><div class="ant-form-item-control "><button class="Action ant-btn-lg" data-next="listingForm" id="next">Next</button></div></div></div></div>
+            <div class="Editor__bottom__right"><div class="ant-row ant-form-item Editor__bottom__next"><div class="ant-form-item-control-wrapper"><div class="ant-form-item-control "></div></div></div><div class="ant-row ant-form-item Editor__bottom__submit"><div class="ant-form-item-control-wrapper"><div class="ant-form-item-control "><button class="Action ant-btn-lg" data-next="listingTypeForm" id="next">Next</button></div></div></div></div>
 </div>              
         </div>
       </div>
