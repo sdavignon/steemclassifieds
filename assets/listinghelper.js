2fd9b7c25b8802bfb12bc2c2a30f9c05cf2d01c3
sdavignon <sdavignon1@gmail.com>
2018-03-08 00:54:56 -0500

    Update listinghelper.js

--- assets/listinghelper.js
+++ assets/listinghelper.js
@@ -73,9 +73,45 @@ function nextForm() {
     $('#next').attr("disabled", false); 
     
     $(".waiting").hide();
-
+googleMap();
   });
 }
+
+function googleMap(){
+ var map = new google.maps.Map(document.getElementById("map"), {
+   center: new google.maps.LatLng(53.3478, -6.2597),
+   zoom: 16,
+   mapTypeId: google.maps.MapTypeId.ROADMAP
+ });
+
+ var infoWindow = new google.maps.InfoWindow({
+   map: map
+ });
+ // Try HTML5 geolocation.
+ if (navigator.geolocation) {
+   navigator.geolocation.getCurrentPosition(function(position) {
+     var pos = {
+       lat: position.coords.latitude,
+       lng: position.coords.longitude
+     };
+     infoWindow.setPosition(pos);
+     infoWindow.setContent('<b>You are here.</b>');
+     map.setCenter(pos);
+     var marker = new google.maps.Marker({
+       position: pos,
+       map: map,
+       title: String(pos.lat) + ", " + String(pos.lng),
+     });
+   }, function() {
+     handleLocationError(true, infoWindow, map.getCenter());
+   });
+ } else {
+   // Browser doesn't support Geolocation
+   handleLocationError(false, infoWindow, map.getCenter());
+ }
+
+}
+
 
 function lastForm() { 
   
